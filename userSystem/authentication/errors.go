652919eba9b6846e87001063fde5ad07c8ca3665
Pawel Bogdanowicz <pawelb021@gmail.com>
2023-07-20 13:04:47 +0200

    changed self made token system to JWT tokens

--- userSystem/authentication/errors.go
+++ userSystem/authentication/errors.go
@@ -2,18 +2,6 @@ package authentication
 
 import "fmt"
 
-type ErrInvalidTokenData struct {
-	Msg string
-}
-
-func (e *ErrInvalidTokenData) Error() string {
-	if e.Msg == "" {
-		return "Invalid authentication token data"
-	} else {
-		return fmt.Sprintf("Invalid authentication token data: %s", e.Msg)
-	}
-}
-
 type ErrInvalidToken struct {
 	Msg string
 }
